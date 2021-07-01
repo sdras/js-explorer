9993b8dbf03a55196cb8aa97ba32b53fa04030e7
Sarah Drasner <sarah.drasner@gmail.com>
2021-07-01 17:40:49 -0600

    💻 make output more clear

--- bin/logFinalAnswer.js
+++ bin/logFinalAnswer.js
@@ -4,8 +4,10 @@ import figures from 'figures'
 import hyperlinker from 'hyperlinker'
 import supportsHyperlinks from 'supports-hyperlinks'
 const log = console.log
+const divider = `\n.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo.oOo`
 
 export function logFinalAnswer(finalAnswer) {
+  log(chalk.blackBright(divider))
   log(chalk.yellowBright.bold.underline(`\nArray.${finalAnswer.name}()`))
   log(finalAnswer.desc + '\n')
 
@@ -15,10 +17,15 @@ export function logFinalAnswer(finalAnswer) {
     borderColor: 'blackBright',
     backgroundColor: '#000000',
   }
-  const code = `  let arr = [5, 1, 8];
-  ${finalAnswer.output}`
+  const code =
+    chalk.blackBright('Usage:\n') +
+    chalk.cyanBright(`let arr = [5, 1, 8];
+  ${finalAnswer.example}\n\n`) +
+    chalk.blackBright('Output:\n') +
+    chalk.cyan(finalAnswer.output)
+
   const codeOutput = boxen(code, codeBox)
-  log(chalk.cyanBright(codeOutput))
+  log(codeOutput)
 
   const mozLink = `https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/${finalAnswer.name}`
 
@@ -31,8 +38,9 @@ export function logFinalAnswer(finalAnswer) {
   } else {
     log(
       chalk.blueBright(
-        chalk.bold(`\ndocs link ${figures.arrowRight} `) + `${mozLink}\n`
+        chalk.bold(`\ndocs link ${figures.arrowRight}\n`) + `${mozLink}\n`
       )
     )
   }
+  log(chalk.blackBright(divider))
 }
