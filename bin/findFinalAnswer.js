986dcfe76837cbedf12636a5fe3bd7555d40b0e4
Sarah Drasner <sarah.drasner@gmail.com>
2021-07-02 13:08:57 -0600

    🍞 comments to find things easily later

--- bin/findFinalAnswer.js
+++ bin/findFinalAnswer.js
@@ -1,6 +1,8 @@
 import { arr } from './arrayMethod.js'
 import { obj } from './objectMethod.js'
 
+//find the rest of the values based on the last response
+//we will have a value in that object, but we need to access the others to display
 function nestedAnswer(type, dataStructure, answers) {
   let answerArr = Object.keys(answers)
   let lastProp = answerArr[answerArr.length - 1]
@@ -16,10 +18,12 @@ export function findFinalAnswer(answers) {
   let answerProps = Object.keys(answers)
   let answerValues = Object.values(answers)
 
+  // find in array and infoProp in object are both deeply nested
   if (answerProps.includes('find')) {
     return nestedAnswer('find', arr, answers)
   } else if (answerProps.includes('infoProp')) {
     return nestedAnswer('infoProp', obj, answers)
+  // otherwise, just figure out if it's an array or object
   } else if (answerValues.includes('array')) {
     return nestedAnswer(null, arr, answers)
   } else {
