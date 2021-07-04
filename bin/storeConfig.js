0f3b9b250dbcd0c16f3046cefd453b00d00d0b1f
Sarah Drasner <sarah.drasner@gmail.com>
2021-07-03 20:15:02 -0600

    update confirm

--- bin/storeConfig.js
+++ bin/storeConfig.js
@@ -24,7 +24,9 @@ export const storeConfig = (finalAnswer) => {
   inquirer.prompt(configQuestion).then((answers) => {
     store.set(answers.namedConfig, finalAnswer)
 
+    if (answers.namedConfig === true) {
     console.log(`\nGreat! This is now stored.
 You can access it anytime by running ` + chalk.magenta('lookup ' + answers.namedConfig) + '\n')
+    }
   })
 }
