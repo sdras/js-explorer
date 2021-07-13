f97913743b087e658f88aa7ef4ee8c27fc85a213
Anand <anand.shenoy14@gmail.com>
2021-07-12 23:03:43 -0700

    :hammer: repairs to package json to involve dirname to locally resolve :file_folder:

--- bin/store.js
+++ bin/store.js
@@ -1,5 +1,9 @@
 import fs from 'fs'
 import Configstore from 'configstore';
-const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+import path from 'path'
+import { fileURLToPath }  from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const packageJson = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../package.json'), 'utf8'));
 
 export const store = new Configstore(packageJson.name, {init: true})
